defbbd27678302e8d0c5426e4ad8e5ae9a320fd1
Tiffany Luu <tiffanyell1@gmail.com>
2020-09-30 23:27:25 +0000

    Added and tokenized user input

--- calculator.py
+++ calculator.py
@@ -3,5 +3,23 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
+def calculator():
 # Replace this with your code
+# #repeat forever:
+    while True:
+#     read input (e.g. + 1 2)
+        nums_input = input("Enter your equation: ")
+#     tokenize input
+        tokens = nums_input.split(' ')
+        print(tokens)
+#         if the first token is "q":
+            
+#             quit
+calculator()
+
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
+
+#             (...etc.)
